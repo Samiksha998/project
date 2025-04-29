f9ad2f34093b5346a50e5900447b9627a89fdc35
EC2 Default User <ec2-user@ip-172-31-22-4.ec2.internal>
2025-04-29 15:45:40 +0000

    code

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "k8s_instance" {
+  ami           = "ami-0e449927258d45bc4" # Amazon Linux 2 AMI
+  instance_type = "t3.medium"
+  key_name      = "key"
+
+  user_data = file("scripts/install-k8s.sh")
+
+  tags = {
+    Name = "k8s-instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.k8s_instance.public_ip
+}
