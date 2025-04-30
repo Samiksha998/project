fd6dfd3de95eb5fc0804c4278d2de946d4947564
Samiksha Gurav <110155865+Samiksha998@users.noreply.github.com>
2025-04-30 14:21:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# EC2 Instance
 resource "aws_instance" "k8s_instance" {
-  ami           = "ami-0b86aaed8ef90e45f" # Amazon Linux 2 AMI
+  ami           = "ami-0b86aaed8ef90e45f"
   instance_type = "t2.medium"
   key_name      = "key"
 
@@ -14,6 +15,17 @@ resource "aws_instance" "k8s_instance" {
   }
 }
 
+# Elastic IP - Associate existing EIP (must be manually allocated beforehand)
+resource "aws_eip" "static_ip" {
+  public_ip = "18.204.224.252"
+
+  # Important: This requires the EIP to exist in your AWS account!
+  depends_on = [aws_instance.k8s_instance]
+
+  instance = aws_instance.k8s_instance.id
+}
+
+# Output for verification
 output "instance_public_ip" {
   value = aws_instance.k8s_instance.public_ip
 }
